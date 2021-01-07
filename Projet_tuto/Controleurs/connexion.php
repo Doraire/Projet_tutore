929cb49fcd39cc4dc0b047b5c4ef1ce2196c0f84
Doraire <77102854+Doraire@users.noreply.github.com>
2021-01-07 12:42:57 +0100

    Update connexion.php

--- Projet_tuto/Controleurs/connexion.php
+++ Projet_tuto/Controleurs/connexion.php
@@ -3,11 +3,19 @@
 	include("../Modeles/bd.php");
 	$login = $_POST["login"];
 	$mdp = $_POST["mdp"];
-	echo "$login $mdp ohhoho";
-	$bd = new bd("projet_tuto");
-	echo "héhéhé";
-	//$co = $bd->connexion();
-	//$requete = "SELECT numClient, loginClient, mdpClient FROM client WHERE loginClient = $login AND mdpClient = $mdp";
-	//$resultat = mysqli_query($co, $result);
-	echo "héhéhé";
+	$bd = new bd();
+	$co = $bd->connexion();
+	$requete = "SELECT numClient, loginClient, mdpClient FROM client WHERE loginClient = '$login' AND mdpClient = '$mdp'";
+	$resultat = mysqli_query($co, $requete);
+	if(mysqli_num_rows($resultat) == 1){
+		$row = mysqli_fetch_assoc($resultat);
+		$numclient = $row["numClient"];
+		$loginClient = $row["loginClient"];
+		$membre = new Membre($co,$numclient,$loginClient);
+		$membre->connexion();
+		header("Location: ../Vues/html/page_index.php");
+	}
+	else{
+		header("Location: ../Vues/html/page_connexion.php?mdp=nega");
+	}
 ?>
