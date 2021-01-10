1459228833e343787f4c70be68a3933e793b9500
Doraire <77102854+Doraire@users.noreply.github.com>
2021-01-10 19:41:42 +0100

    Add files via upload

---
+++ Projet_tuto/Controleurs/ajout_au_panier.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+
+	if (!isset($_SESSION['panier'])){
+      $_SESSION['panier']=array();
+      $_SESSION['panier']['nomProduit'] = array();
+      $_SESSION['panier']['qteProduit'] = array();
+    }
+
+    foreach( $_POST as $cle=>$value)
+	{
+		$flag = 0;
+
+		//Code pour remplacer la quantité si on trouve le même produit
+		
+		for ($i=0 ;$i < count($_SESSION['panier']['nomProduit']); $i++){
+			if($cle == $_SESSION['panier']['nomProduit'][$i]){
+				$_SESSION['panier']['qteProduit'][$i] = $value;
+				$flag = 1;
+			}
+		}
+
+
+		//Ajout des produits dans le panier
+		if($flag==0){
+			array_push($_SESSION['panier']['nomProduit'],$cle);
+        	array_push($_SESSION['panier']['qteProduit'],$value);
+		}
+        
+  	}
+	
+	header("Location: ../Vues/html/page_produits.php");
+?>
