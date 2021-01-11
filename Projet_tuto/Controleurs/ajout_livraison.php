89b49b788f8deca2e866b689b8d28519a1f8b4c2
Doraire <77102854+Doraire@users.noreply.github.com>
2021-01-11 19:25:24 +0100

    Add files via upload

---
+++ Projet_tuto/Controleurs/ajout_livraison.php
@@ -0,0 +1,17 @@
+<?php
+	include("../Modeles/bd.php");
+	$bd = new Bd();
+	$co = $bd->connexion();
+
+	if(isset($_POST['datelivraison'])){
+		$new_date = date('Y-m-d', strtotime($_POST['datelivraison']));
+		$numcommande = $_POST["numcommande"];
+		mysqli_query($co,"INSERT INTO Livraison VALUES (NULL,'$new_date',0)");
+		$id = mysqli_insert_id($co);
+		mysqli_query($co,"INSERT INTO commande_livraison VALUES($numcommande,$id)");
+		
+	}
+
+	header("Location: ../Vues/html/page_commande.php?optadm=1");
+
+?>
