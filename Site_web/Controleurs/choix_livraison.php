5d5836c86581fe5a8852c5e951cfd33dc4c6153f
DelamarcheRuben <77103583+DelamarcheRuben@users.noreply.github.com>
2021-01-12 18:51:32 +0100

    Update choix_livraison.php

--- Site_web/Controleurs/choix_livraison.php
+++ Site_web/Controleurs/choix_livraison.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
+/*session_start();
 $num_client = $_SESSION["num_client"];
 require_once("../Modeles/bd.php");
 $bd = new Bd();
-$co = $bd->connexion();
-
+$co = $bd->connexion();*/
+header("Location: ../Vues/html/page_accueil.php");
 ?>
