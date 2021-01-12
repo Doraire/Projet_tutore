884730ab3e8890950eb035bdd6e0a2e682549d45
Doraire <77102854+Doraire@users.noreply.github.com>
2021-01-12 16:29:00 +0100

    Update ajout_livraison.php

--- Projet_tuto/Controleurs/ajout_livraison.php
+++ Projet_tuto/Controleurs/ajout_livraison.php
@@ -5,10 +5,12 @@
 
 	if(isset($_POST['datelivraison'])){
 		$new_date = date('Y-m-d', strtotime($_POST['datelivraison']));
-		$numcommande = $_POST["numcommande"];
-		mysqli_query($co,"INSERT INTO Livraison VALUES (NULL,'$new_date',0)");
-		$id = mysqli_insert_id($co);
-		mysqli_query($co,"INSERT INTO commande_livraison VALUES($numcommande,$id)");
+		if($new_date!='1970-01-01'){
+			$numcommande = $_POST["numcommande"];
+			mysqli_query($co,"INSERT INTO Livraison VALUES (NULL,'$new_date',0)");
+			$id = mysqli_insert_id($co);
+			mysqli_query($co,"INSERT INTO commande_livraison VALUES($numcommande,$id)");
+		}
 		
 	}
 
