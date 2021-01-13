949afec4d1273b23b7d00cfa6e9fad4adff39464
Doraire <77102854+Doraire@users.noreply.github.com>
2021-01-13 11:15:40 +0100

    Update ajout_au_panier.php

--- Site_web/Controleurs/ajout_au_panier.php
+++ Site_web/Controleurs/ajout_au_panier.php
@@ -6,28 +6,30 @@
       $_SESSION['panier']['nomProduit'] = array();
       $_SESSION['panier']['qteProduit'] = array();
     }
-
+	$numcommande = $_POST["numcommande"];
     foreach( $_POST as $cle=>$value)
 	{
-		$flag = 0;
+		if($cle!="numcommande"){		
+	        	$flag = 0;
 
-		//Code pour remplacer la quantité si on trouve le même produit
+			//Code pour remplacer la quantité si on trouve le même produit
 		
-		for ($i=0 ;$i < count($_SESSION['panier']['nomProduit']); $i++){
-			if($cle == $_SESSION['panier']['nomProduit'][$i]){
-				$_SESSION['panier']['qteProduit'][$i] = $value;
-				$flag = 1;
+			for ($i=0 ;$i < count($_SESSION['panier']['nomProduit']); $i++){
+				if($cle == $_SESSION['panier']['nomProduit'][$i]){
+					$_SESSION['panier']['qteProduit'][$i] = $value;
+					$flag = 1;
+				}
 			}
-		}
 
 
-		//Ajout des produits dans le panier
-		if($flag==0){
-			array_push($_SESSION['panier']['nomProduit'],$cle);
-        	array_push($_SESSION['panier']['qteProduit'],$value);
+			//Ajout des produits dans le panier
+			if($flag==0){
+				array_push($_SESSION['panier']['nomProduit'],$cle);
+        		array_push($_SESSION['panier']['qteProduit'],$value);
+			}
 		}
         
   	}
 	
-	header("Location: ../Vues/html/page_produits.php");
+	header("Location: ../Vues/html/page_produits.php?numcom=$numcommande");
 ?>
